1aff390dd78495048002ad1a63a81280a5622c68
Beomcheol Kwon <bc6793@gmail.com>
2022-04-06 04:00:06 +0900

    20200406

--- sol/12865 평범한 배낭.swift
+++ sol/12865 평범한 배낭.swift
@@ -11,25 +11,19 @@ let nk = readLine()!.split(separator: " ").map{ Int($0)! }
 let n = nk[0]
 let k = nk[1]
 
-var cost = [0]
-var weight = [0]
+var dp = Array(repeating: Array(repeating: 0, count: k+1), count: n+1)
 
-var dp = Array(repeating: 0, count: k+1)
-
-for _ in 0..<n {
+for i in 1..<n+1 {
     let wv = readLine()!.split(separator: " ").map{ Int(String($0))! }
     let w = wv[0]
     let v = wv[1]
-    cost.append(v)
-    weight.append(w)
-}
 
-for i in 1...n {
-    for j in stride(from: k, to: 0, by: -1) {
-        if j >= weight[i] {
-            dp[j] = max(dp[j], dp[j-weight[i]] + cost[i])
+    for j in 1..<k+1 {
+        if j < w {
+            dp[i][j] = dp[i-1][j]
+        } else {
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w]+v)
         }
     }
 }
-
-print(dp[k])
+print(dp[n][k])
